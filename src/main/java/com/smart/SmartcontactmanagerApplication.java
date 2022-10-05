088f7418c40e49794914cc9faf4c362b41fde229
Mahesh Kottawar <100727933+maheshkottawar@users.noreply.github.com>
2022-10-06 00:33:21 +0530

    Update SmartcontactmanagerApplication.java

--- src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -18,15 +18,7 @@ public class SmartcontactmanagerApplication implements CommandLineRunner  {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-//		
-//		String str="durgesh";
-//		String encode = this.bryBCryptPasswordEncoder.encode(str);
-//		System.out.println(encode);		
-//		String str1="durgesh";
-//		String encode2 = this.bryBCryptPasswordEncoder.encode(str1);		
-//		System.out.println(this.bryBCryptPasswordEncoder.matches("durges352h",encode));
-//		
+		
 		
 	}
 
